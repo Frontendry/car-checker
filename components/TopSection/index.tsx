122d310c35d3fd390960b0d3f9daea9f3856c7f7
frontendry <brngatarwa@gmail.com>
2022-07-22 19:50:28 +0300

    Top Section cleanup

--- components/TopSection/index.tsx
+++ components/TopSection/index.tsx
@@ -10,9 +10,23 @@ const TopSection = () => {
           </div>
           <div className="col-lg-8 header-right mt-lg-0 mt-2 d-flex">
             <ul className="ml-auto d-flex align-items-center">
-              <li className="text-center text-white d-flex">
-                <i className="fas fa-phone mr-2"></i>
-                <span>+254 700 000 020</span>
+              <li className="text-center border-right pr-2 mr-3">
+                <a
+                  href="tel:+254 700 000 020"
+                  className="text-white d-flex align-items-center"
+                >
+                  <i className="fas fa-phone mr-2"></i>
+                  <span>+254 700 000 020</span>
+                </a>
+              </li>
+              <li className="text-center">
+                <a
+                  href="mailto:info@carchecker.co.ke"
+                  className="text-white d-flex align-items-center"
+                >
+                  <i className="fas fa-envelope mr-2"></i>
+                  <span>info@carchecker.co.ke</span>
+                </a>
               </li>
             </ul>
           </div>
