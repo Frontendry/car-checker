3a9b64e8ac75227dc03063c7e1466e5a2d477414
frontendry <brngatarwa@gmail.com>
2022-07-24 17:32:16 +0300

    dynamic routing for cars

---
+++ pages/cars/[pid].tsx
@@ -0,0 +1,45 @@
+import { GetStaticProps, GetStaticPaths } from "next";
+import { ParsedUrlQuery } from "querystring";
+
+// Interfaces
+import {
+  CarDetails,
+  CarExtraDetails,
+  CarExtraDetailsProps,
+} from "../../config/interfaces";
+
+// Config
+import axiosModules from "../../config/axios";
+import { CARS_LIST, SINGLE_CAR } from "../../config/constant";
+
+// Axios Instance
+const { axios } = axiosModules;
+
+const CarPost = (props: CarExtraDetailsProps) => {
+  const carDetailsData = props.carDetails;
+  return <div>{carDetailsData.carName}</div>;
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await axios.get(`${CARS_LIST}`);
+  const carslist = await res.data.result;
+
+  const paths = carslist.map((item: CarDetails) => ({
+    params: { pid: item.id },
+  }));
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { pid } = context.params!;
+  const res = await axios.get(`${SINGLE_CAR}/${pid}`);
+  const carDetails = (await res.data) as CarExtraDetails;
+
+  // Pass post data to the page via props
+  return { props: { carDetails } };
+};
+
+export default CarPost;
